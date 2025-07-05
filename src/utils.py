28b04cd56f20c4d5f306b25b1da790514ddfa98a
mignatko <myroslav.ignatko@gmail.com>
2025-07-05 18:35:06 +0200

    Refactored structure and added type hints

---
+++ src/utils.py
@@ -0,0 +1,55 @@
+def get_table_dimentions(first_word_index: int, last_word_index: int, words: list[dict[str, any]]) -> tuple[float, float, float, float]:
+    left: float = words[first_word_index]["x0"]
+    right: float = words[last_word_index]["x1"] + 5
+    top: float = words[first_word_index]["top"]
+    bottom: float = words[last_word_index]["bottom"]
+
+    return (top, left, bottom, right)
+
+def get_first_table_word_index(words: list[dict[str, any]], cardFirstFourNumbers: str, cardLastFourNumbers: str) -> int:
+    top_sequence = ("Card", "number", cardFirstFourNumbers, "XXXX", "XXXX", cardLastFourNumbers)
+    return find_word_adjacent_to_the_sequence((top_sequence,), words)
+
+def get_last_table_word_index(words: list[dict[str, any]], cardFirstFourNumbers: str):
+    bottom_sequence_1 = ("Page", "_", "of")
+    bottom_sequence_2 = ("Total", "for", cardFirstFourNumbers)
+    return find_word_adjacent_to_the_sequence((bottom_sequence_1, bottom_sequence_2), words, adjacent_left=True)
+
+def find_word_adjacent_to_the_sequence(sequences: tuple[tuple[str, ...], ...], words: list[dict[str, any]], adjacent_left: bool=False) -> int:
+    index = -1
+    for sequence in sequences:
+        pointer = 0
+        for i in range(len(words)):
+            word_text = words[i]["text"]
+            if word_text == sequence[pointer] or sequence[pointer] == "_":
+                pointer += 1
+                if pointer == len(sequence):
+                    index = i - len(sequence) if adjacent_left else i + 1
+                    break
+            else:
+                pointer = 0
+
+    if index >= 0 and words[index]["text"] == "Ý":
+        index = index - 1 if adjacent_left else index + 1
+
+    return index
+
+def get_column_positions(table_coords: tuple[float, float, float, float], words: list[dict[str, any]]) -> dict[str, tuple[float, float]]:
+    top_sequence = ("date", "date", "Description", "Spend", "Categories", "Amount($)")
+    index = find_word_adjacent_to_the_sequence((top_sequence,), words, adjacent_left=True)
+
+    if index < 0:
+        raise ValueError("Table headers were not found.")
+    
+    post_date_start: float = words[index + 2]["x0"] - 10
+    description_start: float = words[index + 3]["x0"] - 10
+    category_start: float = words[index + 4]["x0"] - 10
+    amount_start: float = words[index + 6]["x0"] - 10
+
+    return {
+        "transaction_date": (table_coords[1], post_date_start),
+        "post_date": (post_date_start, description_start),
+        "description": (description_start, category_start),
+        "category": (category_start, amount_start),
+        "amount": (amount_start, table_coords[3]),
+    }
